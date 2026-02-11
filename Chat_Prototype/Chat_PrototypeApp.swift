1ec89fa433e41847a2a3eb3f5dae0ae3432a96c0
27hosman-pixel <27hosman@gmail.com>
2026-02-11 10:40:11 -0800

    Initial Commit

---
+++ Chat_Prototype/Chat_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_PrototypeApp.swift
+//  Chat_Prototype
+//
+//  Created by Hana Osman on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Chat_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
