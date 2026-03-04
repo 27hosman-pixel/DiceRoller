80bf2799fc1cb869a5cf4169ab84d99dd19394c4
27hosman-pixel <27hosman@gmail.com>
2026-03-03 23:09:43 -0800

    Finishing touches completed

--- Dice Roller/DiceRoller/DiceRoller/ContentView.swift
+++ Dice Roller/DiceRoller/DiceRoller/ContentView.swift
@@ -14,26 +14,45 @@ struct ContentView: View {
         VStack {
             Text("Dice Roller")
                 .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
             
-
-                
+            
+            HStack{
                 ForEach(1...numberOfDice, id: \.description){ _ in
                     DiceView()
                 }
-    
+                
+            }
+            
+            
+            HStack{
+                Button("Remove Dice", systemImage: "minus.circle.fill"){
+                    withAnimation{
+                        numberOfDice-=1
+                    }
+                    
+                }
+                .disabled(numberOfDice==1)
+                
+                Button("Add Dice", systemImage: "plus.circle.fill"){
+                    withAnimation{
+                        numberOfDice+=1
+                    }
+                    
+                }
+                .disabled(numberOfDice==5)
             }
             .padding()
-        
-        HStack{
-            Button("Remove Dice"){
-                numberOfDice-=1
-            }
-            Button("Add Dice"){
-                numberOfDice+=1
-            }
+            .labelStyle(.iconOnly)
+            .font(.title)
+            
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.appBackground)
+        .tint(.white)
     }
+    
 }
 
 #Preview {
