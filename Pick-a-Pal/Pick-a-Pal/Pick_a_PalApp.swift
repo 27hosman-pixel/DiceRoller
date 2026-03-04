48d193ea64e4099444750f9051f9886773d3763f
27hosman-pixel <27hosman@gmail.com>
2026-03-04 09:12:18 -0800

    Finished

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Hana Osman on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
